2ea972dbb519936e091972fb38fb0ae0e5ee91f4
Krypti <learnaslikefun@gmail.com>
2025-06-23 17:59:59 +0530

    Organize the files of rust Journey.

---
+++ hello_from_rust/PLEASE_README.md
@@ -0,0 +1,13 @@
+# Welcome to the journey
+
+This is common developer journey that's all.
+
+## Hello, everyone
+
+Here i will discuss about git and my projects.
+
+## Hello, Git
+
+This is the start of git with everyone else.
+
+## __________
---
+++ hello_from_rust/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, git!");
+    println!("Hello, rest of the world!");
+    greetings();
+}
+
+fn greetings() {
+    println!("Welcome everyone, just a guy learning rust!");
+    println!("hope you like it.");
+}
+
+fn do_you_like_it() {
+    println!("Yep!")
+}
